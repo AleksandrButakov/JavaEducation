57d1bf6297b87ba46eaa9abc1fabdf081ea6b0d0
Aleksandr Butakov <anbngm@gmail.com>
2022-05-07 15:25:53 +0500

    Added homework 19

--- src/main/java/ru/gazprombank/educate/homework20/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework20/Homework.java
@@ -1,6 +1,20 @@
 package ru.gazprombank.educate.homework20;
 
 public class Homework {
+    /*
+    Рекурсия Yo
+    - сделать статический публичный метод в `HomeWork` под названием `yoYo`
+    - метод должен принимать на вход 1 параметр `String str`
+    - метод должен вернуть сколько раз слово `yo` встречается в исходной строке
+    - решение должно быть выполнено с применением рекурсии, без циклов
+
+    Пример
+    ```
+    yoYo("xhyoxx")   - вернет 1
+    yoYo("nyonnyon") - вернет 2
+    yoYo("yo")       - вернет 1
+    ```
+     */
 
     public static void main(String[] args) {
 
