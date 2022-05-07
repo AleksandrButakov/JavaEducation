ffac4b46e41605ae49e58675cb4f951561374904
Aleksandr Butakov <anbngm@gmail.com>
2022-05-07 14:11:48 +0500

    Added homework 18

--- src/main/java/ru/gazprombank/educate/homework18/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework18/Homework.java
@@ -6,7 +6,8 @@ public class Homework {
     - сделать статический публичный метод в `HomeWork` под названием `matchingChar`
     - метод должен принимать на вход 2 параметра `String[] a, String[] b`
     - строки в массивах могут быть пустыми
-    - необходимо сравнить элементы обоих массивов (первый с первым, второй со вторым и т.д.), и посчитать количество раз, когда первый символ в обеих не пустых строках совпадает
+    - необходимо сравнить элементы обоих массивов (первый с первым, второй со вторым и т.д.), и посчитать
+    количество раз, когда первый символ в обеих не пустых строках совпадает
 
     Пример
     ```
@@ -17,6 +18,28 @@ public class Homework {
      */
 
     public static void main(String[] args) {
-
+        String[] a = {"abc", "bcv", "sd"};
+        String[] b = {"rt", "etr", "ss"};
+        System.out.println("Number of matches = " + matchingChar(a, b));
     }
+
+    public static int matchingChar(String[] a, String[] b) {
+        int lengthArray;
+        int count = 0;
+        if (a.length < b.length) {
+            lengthArray = a.length;
+        } else {
+            lengthArray = b.length;
+        }
+
+        for (int i = 0; i < lengthArray; i++) {
+            if (a[i].length() > 0 && b[i].length() > 0) {
+                if (a[i].charAt(0) == b[i].charAt(0)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
 }
